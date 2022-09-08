6381e62f2163d2fb21d636af5e4a821161c5c1a2
m0nadic <anirbang.m0@gmail.com>
2022-09-08 23:24:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="container my-4">
+      <h1>Hello World</h1>
+      <button className="btn btn-primary">Say Hello</button>
+    </div>
+  );
+}
+
+export default App;
